653d604e874c82763bb6731c7d7f58a3de1d13bf
cteng585 <cteng585@stanford.edu>
2025-02-25 20:40:38 -0800

    feat/refactor: add support forcing subsetting if one of the samples to subset on doesn't exist in the VCF. removed repetitive code.

--- src/vcf_processing/utils.py
+++ src/vcf_processing/utils.py
@@ -5,35 +5,48 @@ from typing import Optional, Union
 from src.vcf_processing.classes import VCF
 
 
-def subset(vcf: VCF, samples: Union[str, list[str]], subset_path: Optional[str, Path]) -> VCF:
+def subset(
+    vcf: VCF,
+    samples: Union[str, list[str]],
+    subset_path: Optional[Union[str, Path]] = None,
+    force: bool = False,
+) -> VCF:
     """
     Use bcftools to subset the VCF file
 
     :param vcf: the VCF to subset
     :param samples: the samples of the VCF to include in the subset
+    :param subset_path: the path to save the subsetted VCF to
+    :param force: whether to try to subset samples that may not exist in the VCF
     :return: a new VCF object with the subsetted samples
     """
     if not subset_path:
-        subset_string = ".".join(samples)
-        subset_path = Path(vcf.path.stem).with_suffix(f".{subset_string}.vcf.gz")
+        subset_string = ".".join(
+            [sample for sample in samples if sample in vcf.samples]
+        )
+        subset_path = Path(vcf.path.stem).with_suffix(
+            f".{subset_string}.vcf.gz" if subset_string else ".empty.vcf.gz"
+        )
     else:
         subset_path = Path(subset_path)
 
+    args = [
+        "bcftools",
+        "view",
+        vcf.path,
+        "-s",
+        samples if isinstance(samples, str) else ",".join(samples),
+        "-o",
+        subset_path,
+        "-O",
+        "b",
+    ]
+
+    if force:
+        args.append("--force-samples")
     subprocess.run(
-        [
-            "bcftools",
-            "view",
-            vcf.path,
-            "-s",
-            samples if isinstance(samples, str) else ",".join(samples),
-            "-o",
-            subset_path,
-            "-O",
-            "b",
-        ],
+        args,
         check=True
     )
 
-    subset_vcf = VCF(subset_path)
-
-    return subset_vcf
+    return VCF(subset_path)
